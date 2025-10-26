e8321020ff5ac9f8a1afcf8038d2a1da5a446857
yawkar <yawkarpov@gmail.com>
2025-10-26 22:55:56 +0300

    enhance: include gtest support through wrap

---
+++ tests/calc/test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include <calc/calc.hpp>
+
+TEST(TemperatureCalcTests, celsius_to_fahrenheit) {
+  EXPECT_EQ(celsius_to_fahrenheit(12.5), 54.5);
+}
---
+++ tests/greeter/test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <greeter/greeter.hpp>
+
+TEST(GreeterTests, ActualNames) {
+  EXPECT_EQ(greet("Michael"), "Hello Michael");
+  EXPECT_EQ(greet("John"), "Hello John");
+}
+
+TEST(GreeterTests, EmptyName) {
+  EXPECT_EQ(greet(""), "Hello ");
+}
