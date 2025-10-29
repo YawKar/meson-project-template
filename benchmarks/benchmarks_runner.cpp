ca571a4da356d3562a597976b4fc7e0f5560a93f
yawkar <yawkarpov@gmail.com>
2025-10-29 16:03:06 +0300

    tests&benches: unify them in binaries with whole linking
    - make one tests_runner to run all tests
    
    - make one benchmarks_runner to run all benchmarks

---
+++ benchmarks/benchmarks_runner.cpp
@@ -0,0 +1,3 @@
+// This file may be empty.
+// The soul purpose is to get some target file for executable().
+// The real 'main()' function comes from benchmark_main_dep.
---
+++ tests/tests_runner.cpp
@@ -0,0 +1,3 @@
+// This file can be empty.
+// Its purpose is to give the executable() target a source file.
+// The real main() comes from gtest_main_dep.
